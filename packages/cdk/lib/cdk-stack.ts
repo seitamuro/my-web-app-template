79a5439f5c7818b9cecc3b19e492575d65b0e050
seitamuro <seitamuro0210@gmail.com>
2024-12-31 22:51:12 +0900

    cloudfrontでのwebページのデプロイを追加

--- packages/cdk/lib/cdk-stack.ts
+++ packages/cdk/lib/cdk-stack.ts
@@ -5,6 +5,10 @@ import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
+  public userPool: cognito.UserPool;
+  public userPoolClient: cognito.UserPoolClient;
+  public identityPool: cognito.CfnIdentityPool;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -60,6 +64,10 @@ export class CdkStack extends cdk.Stack {
         },
       }
     );
+
+    this.userPool = userPool;
+    this.userPoolClient = userPoolClient;
+    this.identityPool = identityPool;
 
     new cdk.CfnOutput(this, "UserPoolId", {
       value: userPool.userPoolId,
--- setup-env.sh
+++ setup-env.sh
@@ -3,3 +3,6 @@ export STACK_NAME="CdkStack"
 export VITE_USER_POOL_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='UserPoolId'].OutputValue" --output text)
 export VITE_USER_POOL_CLIENT_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='UserPoolClientId'].OutputValue" --output text)
 export VITE_IDENTITY_POOL_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='IdentityPoolId'].OutputValue" --output text)
+
+export STACK_NAME="FrontendStack"
+export BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='BucketName'].OutputValue" --output text)
