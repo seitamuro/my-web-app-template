33076f053ab0ca007abc53339c66254eca7effb0
seitamuro <seitamuro0210@gmail.com>
2024-12-31 16:27:53 +0900

    Amplifyの認証を追加

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,10 +1,24 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
+import './index.css';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.REACT_APP_USER_POOL_ID!,
+      userPoolClientId: import.meta.env.REACT_APP_USER_POOL_CLIENT_ID!,
+      identityPoolId: import.meta.env.REACT_APP_IDENTITY_POOL_ID!,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Authenticator>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Authenticator>
 );
---
+++ setup-env.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+export STACK_NAME="CdkStack"
+export VITE_USER_POOL_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='UserPoolId'].OutputValue" --output text)
+export VITE_USER_POOL_CLIENT_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='UserPoolClientId'].OutputValue" --output text)
+export VITE_IDENTITY_POOL_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='IdentityPoolId'].OutputValue" --output text)
